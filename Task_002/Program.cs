1e1bace997a8bfdc2dc6bccce37baa9a5c84eea1
Andrii Korshenko <andriikorshenko@gmail.com>
2022-10-08 20:35:10 +0300

    Some minor fixes

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,13 +1,13 @@
-﻿int[] array = { 1, 2, 3, 4, 4, 56 };
+﻿int[] arr1 = { 1, 2, 3, 4, 4, 56 };
 
-CheckArray(ref array);
+int[] arr2 = CheckArray(arr1);
 
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < arr2.Length; i++)
 {
-    Console.WriteLine(array[i]);
+    Console.WriteLine(arr2[i]);
 }
 
-static void CheckArray(ref int[] array)
+int[] CheckArray(int[] array)
 {
     for (int i = 1; i < array.Length; i++)
     {
@@ -16,9 +16,10 @@ static void CheckArray(ref int[] array)
             RemoveADuplicateFromArray(i - 1, ref array);
         }
     }
+    return array;
 }
 
-static void RemoveADuplicateFromArray(int indexToDelete, ref int[] array)
+void RemoveADuplicateFromArray(int indexToDelete, ref int[] array)
 {
     int[] newArray = new int[array.Length - 1];
 
