c95ff8bc78bb9f3d0b51eb4b709429a6e835e22a
Andrii Korshenko <andriikorshenko@gmail.com>
2022-10-08 11:44:32 +0300

    The second task is done

---
+++ Task_002/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] array = { 1, 2, 3, 4, 4, 56 };
+
+CheckArray(ref array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+static void CheckArray(ref int[] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] == array[i - 1])
+        {
+            RemoveADuplicateFromArray(i - 1, ref array);
+        }
+    }
+}
+
+static void RemoveADuplicateFromArray(int indexToDelete, ref int[] array)
+{
+    int[] newArray = new int[array.Length - 1];
+
+    for (int i = 0; i < indexToDelete; i++)
+    {
+        newArray[i] = array[i];
+    }
+
+    for (int i = indexToDelete; i < newArray.Length; i++)
+    {
+        newArray[i] = array[i + 1];
+    }
+
+    array = newArray;
+}
