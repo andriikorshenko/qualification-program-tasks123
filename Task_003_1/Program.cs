d7dfee88d91c48776755d339c1b22cd45b62b597
Andrii Korshenko <andriikorshenko@gmail.com>
2022-10-08 13:26:07 +0300

    Third task added with two parts

---
+++ Task_003_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Linear version: Time-complexity is O(n), Space-complexity is O(1);
+int findItem = 10;
+
+Console.WriteLine(FiboLinear(findItem));
+
+int FiboLinear(int number)
+{
+    if (number == 0)
+        return 0;
+    else if (number <= 2)
+        return 1;
+
+    int[] arr = { 0, 1 };
+    int result = default;
+
+    while (number-- > 2)
+    {
+        result = arr[0] + arr[1];
+        arr[0] = arr[1];
+        arr[1] = result;
+    }
+    return result;
+}
---
+++ Task_003_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Recursion version: Time-complexity is O(2^n), Space-complexity is O(n);
+int findItem = 10;
+
+Console.WriteLine(FiboRecursion(findItem));
+
+int FiboRecursion(int number)
+{
+    if (number == 0)
+        return 0;
+    else if (number <= 3)
+        return 1;
+
+    return FiboRecursion(number - 1) + FiboRecursion(number - 2);
+}
