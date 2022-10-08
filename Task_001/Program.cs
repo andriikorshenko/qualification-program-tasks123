9c55282941e05230cf2b39957088c188fc16d08c
Andrii Korshenko <andriikorshenko@gmail.com>
2022-10-08 11:15:36 +0300

    First Task is Done

---
+++ Task_001/Program.cs
@@ -0,0 +1,20 @@
+﻿string text = $"The “C# Professional” course includes the topics " +
+    $"I discuss in my CLR via C# book and teaches how the CLR works " +
+    $"thereby showing you how to develop applications and reusable components " +
+    $"for the .NET Framework.";
+
+string[] str = text.Split(' ');
+
+var groups = str.Distinct()
+    .OrderBy(x => x.Length)
+    .GroupBy(y => y.Length);
+
+foreach (var group in groups)
+{
+    Console.WriteLine($"Words of length: {group.Key}, Count: {group.Count()}");
+
+    foreach (var word in group)
+    {
+        Console.WriteLine(word);
+    }
+}
